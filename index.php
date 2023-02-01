c2ff1f0dd90f11165e1701fa3248e05b4c4d84db
Nina Pantelic <nina.pantelic20@gmail.com>
2023-02-01 14:01:28 +0100

    Napravljena pocetna index.php stranica

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, intitial-scale=1.0">
+    <title>Beauty Salon Nina</title>
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" />
+</head>
+
+<body>
+
+    <?php
+    include 'headings/header.php';
+    ?>
+
+    <div id="first" class="row page" style="background-color: white;">
+        <div class="col-md-5">
+            <div class="row">
+                <img src="img/another.png" class="img-fluid" alt="Photo 1">
+            </div>
+            <div id="description" class="row">
+                <p>
+                    As professionals at Salon Nina our focus is extraordinary quality.
+                    We take our passion and nurture it with a constantly evolving education. <br>
+                    This leaves our guests with an unforgettable experience at Salon Estetica. We are the future. <br>
+
+                    We will provide this level of guest satisfaction while being earth responsible as well. <br> These are the steps we are taking at Salon Nina</p>
+                </p>
+            </div>
+        </div>
+
+        <div class="col-md-7">
+            <div class="row">
+                <img src="img/final.jpg" class="img-fluid" alt="Photo 2">
+            </div>
+            <div id="description2" class="row">
+                <p>
+                    <br>
+                    Enter and enjoy <br>
+                    Our special services <br>
+                    <br>
+                    Your Nina Team
+
+                </p>
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+    <?php
+    include 'headings/footer.php';
+    ?>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"></script>
+</body>
+
+</html>
