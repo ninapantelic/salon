e3892988f2f8f2cf38b8761cb8375d75994de53e
Nina Pantelic <nina.pantelic20@gmail.com>
2023-02-01 10:14:07 +0100

    Header

---
+++ headings/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="collapse" id="navbarToggleExternalContent">
+        <div class="p-4" style="background-color: #480263;">
+            <a style="text-decoration:none; overflow-y: hidden;" href="index.php">
+                <h5 class="text-white h4">Home page</h5>
+            </a>
+            <a style="text-decoration:dotted; overflow-y: hidden;" href="login.php">
+                <h5 class="text-white h4">Login</h5>
+            </a>
+
+            <a style="text-decoration:dotted; overflow-y: hidden;" href="main.php">
+                <h5 class="text-white h4">Make an appointment</h5>
+            </a>
+
+
+        </div>
+    </div>
+    <nav class="navbar navbar-dark" style="background-color: #480263;">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+        </div>
+    </nav>
+</body>
+
+</html>
