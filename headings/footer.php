cb026df31ea9c5f2260973ff5c58e3538e55500a
Nina Pantelic <nina.pantelic20@gmail.com>
2023-02-01 10:14:30 +0100

    Footer

---
+++ headings/footer.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <footer class=" text-light pt-5 pb-4" style="background-color: #480263;">
+
+        <div class="container text-center text-md-left">
+
+            <div class="row text-center text-md-left">
+
+
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text mb-4 font-weight-bold text-third">ABOUT US</h5>
+                    <hr class="mb-4">
+                    <p>
+                        Beauty Salon Nina is a place where all your wishes and requests will be fulfilled with the highest level of service and quality. From the very beginning, our mission is your comfort, convenience and satisfaction.
+
+                    </p>
+
+                </div>
+
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text mb-4 font-weight-bold text-third">SERVICES</h5>
+                    <hr class="mb-4">
+                    <p>
+                        <a href="#" class="text-light" style="text-decoration: none;"> Hair cutting</a>
+                    </p>
+
+                    <p>
+                        <a href="#" class="text-light" style="text-decoration: none;"> Manicure</a>
+                    </p>
+
+                    <p>
+                        <a href="#" class="text-light" style="text-decoration: none;"> Depilation</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-light" style="text-decoration: none;"> ...and many others</a>
+                    </p>
+                </div>
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text mb-4 font-weight-bold text-third">OUR CONTACT</h5>
+                    <hr class="mb-4">
+                    <p>
+                        <i class="fas fa-home mr-3"></i> 5th Avenue
+                    </p>
+                    <p>
+                        <i class="fas fa-envelope mr-3"></i> beautysalonnina@gmail.com
+                    </p>
+                    <p>
+                        <i class="fas fa-phone mr-3"></i> +036525626
+                    </p>
+                    <p>
+                        <i class="fas fa-phone mr-3"></i> +123654892
+                    </p>
+                </div>
+
+            </div>
+
+            <hr class="mb-4">
+            <div class=" row d-flex justify-content-center">
+                <div>
+                    <p> Copyright ©2023 By Nina </p>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="text-center ">
+
+                    <ul class="list-unstyled list-inline">
+                        <li class="list-inline-item">
+                            <a href="#" class="text-light"><i class="fab fa-instagram"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" class="text-light"><i class="fab fa-twitter"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" class="text-light"><i class="fab fa-snapchat"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" class="text-light"><i class="fab fa-facebook"></i></a>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        </div>
+</body>
+
+</html>
